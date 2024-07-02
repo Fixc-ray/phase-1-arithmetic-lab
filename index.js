83fa7ad9c644b23f1bfe2d2aa8a7e6f723a09af5
Justin <rayjustin481@gmail.com>
2024-07-02 21:01:10 +0300

    calculations

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+const multiply = (num1 * num2)
+
+const random = math.round(math.random (0)* 10) +1;
+
+let num3 = 16;
+let num4 = 3;
+const mod = num3 % num4;
+console.log(mod);
+
+const max = math.max(3, 4, 16, 18, 20)
+console.log(max);
